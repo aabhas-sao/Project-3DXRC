78a9d4a04633f6bfa0cb4073cd39f42a358a0ae5
Aabhas <aabhassao0@gmail.com>
2020-10-29 18:48:58 +0530

    adding main menu and welcome scene

---
+++ Project-3DXRC/Assets/scripts/Enter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Return)) {
+            SceneManager.LoadScene("Game Menu");
+        }
+    }
+}
---
+++ Project-3DXRC/Assets/scripts/endGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
