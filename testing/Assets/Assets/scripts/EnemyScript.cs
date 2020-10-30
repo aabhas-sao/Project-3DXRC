7aa4c7b6a3493a80b98f11b19f6146d26683f33b
Aabhas <aabhassao0@gmail.com>
2020-10-30 19:05:59 +0530

    updated welcome scene fonts and colors.

---
+++ testing/Assets/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 30f, rotatingSpeed = 8f;
+
+    private GameObject target;
+    private Rigidbody enemyRb;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+            return;
+        }
+        Vector3 pointTarget = transform.position - target.transform.position;
+        pointTarget.Normalize();
+
+        float value = Vector3.Cross(pointTarget, transform.forward).y;
+        enemyRb.angularVelocity = rotatingSpeed * value * new Vector3(0, 1, 0);
+        enemyRb.velocity = transform.forward * speed;
+    }
+}
---
+++ testing/Assets/Assets/scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pause_ui, settings_ui, score_ui, game_over_ui, player;
+    public bool alive;
+    public int isPaused, explosions, multiplier;
+    float timer, score;
+    public Text score_live, score_end, explosion_count, total;
+
+    void Start()
+    {
+        isPaused = 0;
+        multiplier = 1;
+        timer = 0f;
+        pause_ui = GameObject.FindWithTag("pause_menu");
+        settings_ui = GameObject.FindWithTag("settings_ui");
+        score_ui = GameObject.FindWithTag("score_ui");
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(alive)
+        {
+            timer += Time.deltaTime * multiplier;
+            score_ui.SetActive(true);
+            score_live.text = (timer).ToString("0");
+        }
+        else
+        {
+            game_over_ui.SetActive(true);
+            score_end.text = (timer).ToString("0");
+            explosion_count.text = explosions.ToString("0");
+            total.text = (timer + explosions).ToString("0");
+        }
+    }
+
+    public void Pause()
+    {
+        if(isPaused==0 || isPaused==2)
+        {
+            settings_ui = GameObject.FindWithTag("settings_ui");
+            Debug.Log("Paused (PauseMenu)");
+            Time.timeScale = 0f;
+            pause_ui.SetActive(true);
+            score_ui.SetActive(false);
+            settings_ui.SetActive(false);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if(isPaused==1)
+        {
+            isPaused = 0;
+            Debug.Log("Resumed");
+            pause_ui.SetActive(false);
+            score_ui.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Restarted");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+        settings_ui.SetActive(true);
+        pause_ui.SetActive(false);
+        isPaused=2;
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("MainMenu");
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
