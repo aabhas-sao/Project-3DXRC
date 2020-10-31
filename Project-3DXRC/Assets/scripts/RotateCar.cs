2980c26042854fe430ffedecf395127bdcc668f9
Aabhas <aabhassao0@gmail.com>
2020-11-01 00:09:12 +0530

    adding the car selection scene

---
+++ Project-3DXRC/Assets/scripts/RotateCar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateCar : MonoBehaviour
+{
+    public float rotateSpeed = 4f;
+    public Vector3 angle = new Vector3(1, 1, 1);
+    void Update()
+    {
+        transform.Rotate(angle * Time.deltaTime * rotateSpeed);
+    }
+}
