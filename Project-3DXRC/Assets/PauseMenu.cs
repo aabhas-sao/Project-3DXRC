73f2481b12adcaeb373c06ab7c853f74f76ba492
Aabhas <aabhassao0@gmail.com>
2020-10-31 13:11:35 +0530

    making the pause ui and game over ui

---
+++ Project-3DXRC/Assets/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pause_ui;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume() {
+        pause_ui.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+        pause_ui.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
---
+++ Project-3DXRC/Assets/scripts/GoToMenu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoToMenu : MonoBehaviour
+{
+    public void OpenMenu() {
+        SceneManager.LoadScene("Game Menu");
+    }
+}
