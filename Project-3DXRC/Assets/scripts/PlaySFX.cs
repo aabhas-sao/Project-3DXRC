7261cdaf317529481350b2b3feee402b3aa53fb3
Aabhas <aabhassao0@gmail.com>
2020-11-01 17:49:59 +0530

    adding audio effects in menus

---
+++ Project-3DXRC/Assets/scripts/PlaySFX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySFX : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter(PointerEventData eventData) {
+        FindObjectOfType<AudioManager>().Play("hoverVFX");
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        FindObjectOfType<AudioManager>().Play("click");
+        Debug.Log("click");
+    }
+}
