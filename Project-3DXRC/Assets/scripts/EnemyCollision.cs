86d639e7bdf694b693602f00a7faa9edc0168e9b
Aabhas <aabhassao0@gmail.com>
2020-11-03 11:42:03 +0530

    deleting Enemy Script

--- Project-3DXRC/Assets/scripts/EnemyCollision.cs
+++ Project-3DXRC/Assets/scripts/EnemyCollision.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyCollision : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision) {
         if(collision.collider.tag == "enemy") {
