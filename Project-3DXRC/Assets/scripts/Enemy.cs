c1d98195ba51582a0f829df667290a7e573a8d24
Aabhas <aabhassao0@gmail.com>
2020-11-03 11:38:54 +0530

    enemyCollision Script

---
+++ Project-3DXRC/Assets/scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision) {
+        if(collision.collider.tag == "enemy") {
+            Destroy(gameObject);
+        }
+    }
+}
