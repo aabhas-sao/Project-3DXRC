387b72eec1818733b21886f7386cf90d0c222195
Aabhas <aabhassao0@gmail.com>
2020-11-24 13:45:53 +0530

    intro done

---
+++ Project-3DXRC/Assets/scripts/intro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class intro : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += WelcomeScene;    
+    }
+
+   void WelcomeScene(UnityEngine.Video.VideoPlayer vp) {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       Debug.Log("hi");
+   }
+}
