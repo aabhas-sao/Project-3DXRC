f05741f5ee878bb65a1d1f4c35e4316b280d54d3
Aabhas <aabhassao0@gmail.com>
2020-11-26 00:02:07 +0530

    difficulty changes and intro working now

---
+++ Project-3DXRC/Assets/scripts/splash_manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class splash_manager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"logo compressed.m4v"); 
+        videoPlayer.Play();
+        StartCoroutine(wait());
+    }
+
+    IEnumerator wait ()
+    {
+        yield return new WaitForSeconds(9f);
+        SceneManager.LoadScene(1);
+    }
+    
+}
